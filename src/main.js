1b48a2b1b799d5c76592a244f17531523b524f11
Bart M <bartlomejmigacz@gmail.com>
2025-06-06 18:11:53 +0200

    fetch and upload atricle

--- src/main.js
+++ src/main.js
@@ -1,24 +1,71 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const fetchData = async () => {
+  try {
+    const data = await fetch(
+      "https://bucsktwhwtumetpopcxn.supabase.co/rest/v1/article?select=*",
+      {
+        headers: {
+          apiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ1Y3NrdHdod3R1bWV0cG9wY3huIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgxODEwMDgsImV4cCI6MjA2Mzc1NzAwOH0.2iLepWZTbjck62QE7C5SqNCrEkXamwj7eEMyfmekTJA",
+        },
+      }
+    );
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1 class="text-2xl font-bold text-primary">Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button" class="bg-blue-300 p-4"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+    let translateddata = await data.json();
+    console.log(translateddata)
+    translateddata.forEach(element => {
+      const div = document.createElement("div")
+      const p = document.createElement("p")
+      const p1 = document.createElement("p")
+      const p2 = document.createElement("p")
+      const p3 = document.createElement("p")
+      const p4 = document.createElement("p")
+      const autor = `Author: ${element.author}`
+      p.innerHTML = autor
+      div.appendChild(p)
+      const title = `Title: ${element.title}`
+      p1.innerHTML = title
+      div.appendChild(p1)
+      const subtitle = `Subtitle: ${element.subtitle}`
+      p2.innerHTML = subtitle
+      div.appendChild(p2)
+      const created = `Created at: ${element.created_at}`
+      p3.innerHTML = created
+      div.appendChild(p3)
+      const content = `Content: ${element.content}`
+      p4.innerHTML = content
+      div.appendChild(p4)
+      document.getElementById("app").appendChild(div)
+    });
 
-setupCounter(document.querySelector('#counter'))
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+window.onload = async () => {
+  await fetchData()
+}
+const insert = async () => {
+const datatosend={
+  title:document.getElementById("title").value,
+  subtitle:document.getElementById("subtitle").value,
+  author:document.getElementById("author").value,
+  content:document.getElementById("content").value,
+}
+  try {
+    const data = await fetch(
+      "https://bucsktwhwtumetpopcxn.supabase.co/rest/v1/article",
+      {
+        method: "POST",
+        headers: {
+          apiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ1Y3NrdHdod3R1bWV0cG9wY3huIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgxODEwMDgsImV4cCI6MjA2Mzc1NzAwOH0.2iLepWZTbjck62QE7C5SqNCrEkXamwj7eEMyfmekTJA",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(datatosend),
+      }
+    );
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+document.getElementById("addbutton").addEventListener("click",async (e)=>{
+  await insert()
+})
